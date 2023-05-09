134d1cf85ff53d02a8348b9cc260bd04a62e961e
binwang-neu <binge1638@163.com>
2023-05-08 22:23:19 -0400

    1.add nginx web server; 2.add schedule task to remove expired sessions;

--- server/routes/mails.js
+++ server/routes/mails.js
@@ -4,6 +4,9 @@ const nodemailer = require("nodemailer");
 var express = require('express')
 var router = express.Router()
 const Authcode = require('../model/Authcode')
+const fetch = require('node-fetch')
+const schedule = require('node-schedule')
+
 const leandro = (nome, frontEndDev) => {
 	return {
 		nome,
@@ -11,22 +14,79 @@ const leandro = (nome, frontEndDev) => {
 	}
 }
 
+let access_token = "ya29.a0AWY7CkkGpXCrF7RRQidxs7A5JzVdJtWdgVzPvciTw0dzZ41TloYfsyuwYnsFWjn5kypvxfAFwTjLo_PxEJfnT-cAfNLDOhpaGKf26DhvTHGJ6qdd9-F7K6kjw_YkmdNMh9vCaCZLE2s4oUzBgqCsT5aqHOK8344aCgYKAYASARESFQG1tDrpDYSQuT0qQ6xHrmL_dWVd8g0166"
+
+const scheduleCronstyle = () => {
+  //: schedule task will be executed every 30 seconds
+  schedule.scheduleJob('*/30 * * * *', () => {
+    console.log('scheduleCronstyle:' + new Date())
+    fetch('https://accounts.google.com/o/oauth2/token', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application.json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        client_id: "672020294536-kcj0vcfsu5ptuuisl55ohojg2q5s3ddn.apps.googleusercontent.com",
+        client_secret: "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",
+        refresh_token: "1//04ct6yhL547G7CgYIARAAGAQSNwF-L9IrLXIPRVn7DipFJRadKdZYd92iX7ExZr6XUSG-_RdDuSuGo8BPWahwfMsWyagee6rFXkU",
+        grant_type: "refresh_token"
+      })
+    }).then(res => res.json()).then(json => {
+      console.log(json)
+      if (json.access_token != undefined) {
+        access_token = json.access_token
+        console.log("access token: "+ access_token)
+      } else {
+        console.log("error: "+ json.error)
+      }
+    });
+  });
+}
+scheduleCronstyle();
+
 // 发送邮件
 // Get replay test
 router.get('/send/:to', (req, res) => {
   console.log(req.params.to);
-  let transporter = nodemailer.createTransport({
-    host: 'smtp.163.com',
-    // service: "qq", // 使用了内置传输发送邮件 查看支持列表：https://nodemailer.com/smtp/well-known/
-    // port: 25, // SMTP 端口
+  
+  // 如果使用 Gmail 邮箱
+  // let access_token = "ya29.a0AWY7CklErgyfCQb4HtlaZaDwwU4lT3cX0WePvDrALcnMQdtY4IiaMfIDPrpnz-e0HeAvANdKvqe-HRLOZImyTFBf3CcVvg2GFRh56_9OvY-pbTCAf8Bso51UT4SfHeQ8VpeFeS70ECOUiUr0hfeApr20ctk8aCgYKAZISARESFQG1tDrpdBDAzL5gqhMNpPVgB7I3SA0163";
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
     secure: true,
-    //secureConnection: true, // 使用了 SSL
     auth: {
-      user: "blockModel@163.com",
-      pass: "", //授权码，并非QQ密码
+      type: "OAuth2",
+      user: "models.research.lab@gmail.com",
+      clientId:
+        "672020294536-kcj0vcfsu5ptuuisl55ohojg2q5s3ddn.apps.googleusercontent.com",
+      clientSecret: "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",
+      refreshToken:
+        "1//04ct6yhL547G7CgYIARAAGAQSNwF-L9IrLXIPRVn7DipFJRadKdZYd92iX7ExZr6XUSG-_RdDuSuGo8BPWahwfMsWyagee6rFXkU",
+      accessToken:
+        access_token,
     },
   });
-  
+
+  // let access_token = "ya29.a0AWY7CklvWWtJxwRDHVzfCWu8wuX8tXmlLsDzaDKQvPyfnaSKolYQzDIrVtpWdqxMA3yh2m8GmaBMf799TaPpvwvNGSm1MlE14ZsjEkgi_4HW_3Fsps6_-oA6ilBJnP-Na-SEX5yQ06LRtsnsTJWvNNDwY5s-_ToaCgYKAaoSARESFQG1tDrp5tAitNX2aXh5ZwRZ9zaDiQ0166";
+  // const transporter = nodemailer.createTransport({
+  //   host: "smtp.gmail.com",
+  //   port: 465,
+  //   secure: true,
+  //   auth: {
+  //     type: "OAuth2",
+  //     user: "binwang.neu@gmail.com",
+  //     clientId:
+  //       "569830411778-t4mtq59bkkq4b65pj649m6kdt4irei4k.apps.googleusercontent.com",
+  //     clientSecret: "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",
+  //     refreshToken:
+  //       "1//04ZFWwik31BFOCgYIARAAGAQSNwF-L9IrhCONVYtoRyiIP4HKeaT3Sfj2OHSoa24MKz06hgEtwdM_Fr2FSXSkUb9w-M1xDvkz5tM",
+  //     accessToken:
+  //       access_token,
+  //   },
+  // });
+
   const randomFns=()=> { // 生成6位随机数
     let code = ""
     for(let i= 0;i<6;i++){
@@ -39,27 +99,20 @@ router.get('/send/:to', (req, res) => {
   
   // 拼接验证码
   let text  ='Your verification code is: ' + code + ' .the verification code is valid within 5 minutes';
-  
-  // 收件人地址 (由前端传过来)
+
+  // 如果要使用 Gmail 邮箱
   let mailOptions = {
-    from: 'blockModel@163.com', // 发送地址
+    from: 'models.research.lab@gmail.com', // 发送地址
     to: req.params.to, // 接收列表（可多个）
     subject: "Hello,You are registering for a BlockModel account!", // 主题
     // 发送text或者html格式（任选一个）
     text: text,
   };
-
-  // 如果要使用 Gmail 邮箱
-  // let mailOptions = {
-  //   from: 'models.research.lab@gmail.com', // 发送地址
-  //   to: req.params.to, // 接收列表（可多个）
-  //   subject: "Hello,You are registering for a BlockModel account!", // 主题
-  //   // 发送text或者html格式（任选一个）
-  //   text: text,
-  // };
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      res.json(error);
+      console.log(error)
+      res.json(' '+error)
+      return
     }
     // auth code should be store into authcodes table
     let value = {"_user":req.params.to,"_code":code,"_createtime":Date.now()}
@@ -69,7 +122,7 @@ router.get('/send/:to', (req, res) => {
         res.json(info)
       })
       .catch(err => {
-        res.send('Error : ' + err)
+        res.json('Error: ' + err)
       })
   });
 })
@@ -136,7 +189,6 @@ router.post('/authcode', (req, res) => {
   }
 })
 
-
 module.exports = router
 // 整体流程：
 // 1. 前端用户填入邮箱后，点击发送验证码按钮，生成随机验证码并发到指定邮箱，同时存邮箱、验证码、时间戳到数据库（5min内有效）
@@ -144,18 +196,3 @@ module.exports = router
 // 时间充裕可以增加多少分钟后才能重发的限制
 // 3. 前端接受用户输入的验证码，传给后端验证，若超时直接验证失败
 // 4. 验证通过则邮箱登陆成功，失败则显示验证失败
-
-
-// 如果使用 gmail 发送邮件需要定制刷新 access token，但是这段代码应该是要写在一个定时任务里，还没写定时任务
-// const res = await ctx.curl("https://accounts.google.com/o/oauth2/token", {
-//   method: "POST",
-//   contentType: "json",
-//   dataType: "json",
-//   data: {
-//     client_id: clientId,
-//     client_secret: clientSecret,
-//     refresh_token: refreshToken,
-//     grant_type: "refresh_token",
-//   },
-// });
-// const { access_token } = res.data || {};
